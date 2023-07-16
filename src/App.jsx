e33b9ceb095d0a98af78981f565177da2a805b14
harshvsharma03 <harshvardhan.sharma.che21@itbhu.ac.in>
2023-07-16 15:13:51 +0530

    client

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
+import { useContext } from "react";
 import "./app.scss"
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
-import Login from "./pages/login/Login";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import { AuthContext } from "./authContext/AuthContext";
 
 const App = () => {
-  return <Home/>;
+  const { user }= useContext(AuthContext);
+  return (
+    <Router>    
+        <Routes>
+                <Route exact path="/" element={user?<Home />:<Navigate to="/register"/>} />
+                <Route path="/login" element={!user?<Login />:<Navigate to="/"/>} />
+                <Route path="/register" element={!user?<Register />:<Navigate to="/"/>} />
+          			<Route path="/movies" element={user?<Home type="movie" />:<Navigate to="/register"/>} />
+                <Route path="/series" element={user?<Home type="series" />:<Navigate to="/register"/>} />
+                <Route path="/watch" element={user?<Watch />:<Navigate to="/register"/>} />
+        </Routes>
+    </Router>
+  );
 };
 
-export default App;
+export default App; 
+
+/* 
+<Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+          <Route path="/movies">
+            <Home type="movies"/>
+          </Route>
+          <Route path="/series">
+            <Home type="series"/>
+          </Route>
+        </Switch>
+
+*/
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,47 +1,53 @@
-import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import React, { useContext } from "react";
+import "./navbar.scss"
+import { ArrowDownward, Notifications, Search } from "@material-ui/icons";
 import { useState } from "react";
-import "./navbar.scss";
-
+import {Link} from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
 const Navbar = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
-  return (
-    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
-      <div className="container">
-        <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt=""
-          />
-          <span>Homepage</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New and Popular</span>
-          <span>My List</span>
-        </div>
-        <div className="right">
-          <Search className="icon" />
-          <span>KID</span>
-          <Notifications className="icon" />
-          <img
-            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-          />
-          <div className="profile">
-            <ArrowDropDown className="icon" />
-            <div className="options">
-              <span>Settings</span>
-              <span>Logout</span>
+    const [isScrolled,setIsScrolled] = useState(false);
+    const {dispatch} = useContext(AuthContext)
+    window.onscroll = ()=>{
+        setIsScrolled( window.pageYOffset===0? false:true);
+        return ()=> (window.onscroll = null);
+    }
+    return(
+        <div className={isScrolled ? "navbar scrolled":"navbar" }>
+            <div className="container">
+                <div className="left">
+                    <img 
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHViINwGA-Qgaqqen16iqUGf3P8pTWeXo3ByR6JZwr&s" 
+                    alt="" />
+                    <Link to="/" className="link">
+                    <span>Homepage</span>
+                    </Link>
+                    <Link to="/series" className="link">
+                    <span>Series</span>
+                    </Link>
+                    <Link to="/movies" className="link">
+                    <span>Movies</span>
+                    </Link>
+                    <span>New and Popular</span>
+                    <span>My List</span>
+                </div>
+                <div className="right">
+                    <Search className="icon"/>
+                    <span>KID</span>
+                    <Notifications className="icon"/>
+                    <img src="https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png" 
+                    alt="logo" />
+                    <div className="profile">
+                       <ArrowDownward className="icon"/>
+                       <div className="options">
+                        <span>Settings</span>
+                        <span onClick={()=>dispatch(logout())}>Logout</span>
+                       </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
+    )
+}
+ 
 export default Navbar;
